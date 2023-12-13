f3b65d7860887c5af5fc331841d6fa5ac6105bdb
Nimantha Cooray <nimanthacooray@gmail.com>
2023-12-13 19:08:27 +0530

    Add a way to signal consumers in BlockingQueue
    Implemented a mechanism to signal consumers that the producer is done.

--- src/BlockingQueue.h
+++ src/BlockingQueue.h
@@ -2,6 +2,7 @@
 #include <condition_variable>
 #include <mutex>
 #include <queue>
+#include <optional>
 
 template <typename T>
 class BlockingQueue
@@ -10,25 +11,31 @@ private:
     std::queue<T> m_queue;
     std::mutex m_mutex;
     std::condition_variable m_cond;
+    bool isProducerDone{false};
 
 public:
     void push(T item)
     {
         std::unique_lock<std::mutex> lock(m_mutex);
         m_queue.push(item);
-        m_cond.notify_one();
+        m_cond.notify_all();
     };
 
-    T pop()
+    std::optional<T> pop()
     {
         std::unique_lock<std::mutex> lock(m_mutex);
-        while (m_queue.empty())
+        while (m_queue.empty() && !isProducerDone)
         {
             m_cond.wait(lock);
         }
-        T item = m_queue.front();
-        m_queue.pop();
-        return item;
+
+        if (!m_queue.empty())
+        {
+            T item = m_queue.front();
+            m_queue.pop();
+            return item;
+        }
+        return std::nullopt;
     };
 
     bool empty()
@@ -36,4 +43,11 @@ public:
         std::unique_lock<std::mutex> lock(m_mutex);
         return m_queue.empty();
     };
+
+    void setDone()
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        isProducerDone = true;
+        m_cond.notify_all();
+    }
 };
